a46a1d378e1c34f54f70c82eb47f4b93a295b236
unknown <chandrukannaiah1302@gmail.com>
2025-02-21 16:22:12 +0530

    bidings

---
+++ backend/bid/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from database.models import Bid
+class BidSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Bid
+        fields=['id','listing','bid_price','quantity']    
--- backend/bid/views.py
+++ backend/bid/views.py
@@ -3,7 +3,66 @@ from rest_framework.views import APIView
 from django.http import JsonResponse
 # Create your views here.
 from database.models import Bid
+from .serializers import BidSerializer
+from rest_framework.generics import ListAPIView,RetrieveAPIView,ListCreateAPIView
+from rest_framework.mixins import ListModelMixin,CreateModelMixin,RetrieveModelMixin,UpdateModelMixin,DestroyModelMixin
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+class BidListView(ListAPIView):
+    def get_queryset(self):
+        return Bid.objects.all()
+    
+    serializer_class=BidSerializer
+class BidView(ListModelMixin,CreateModelMixin,RetrieveModelMixin,UpdateModelMixin,GenericAPIView,DestroyModelMixin):
+    # permission_classes=[IsAuthenticated]
+    # authentication_classes=[JWTAuthentication]
 
+    def get_queryset(self):
+        return Bid.objects.filter(buyer=self.request.user)
+    
+    serializer_class=BidSerializer
+    def get(self,request,pk):
+
+        return self.retrieve(request)
+    def post(self,request):
+        return self.create(request) 
+    def put(self,request,pk):
+        return self.update(request)
+    def delete(self,request,pk):
+        return self.destroy(request)
+    
+
+# class BidView(RetrieveAPIView):
+#     def get_queryset(self):
+#         _id=self.kwargs.get("pk")
+#         bid=Bid.objects.filter(id=_id)
+#         return bid
+        
+#     serializer_class=BidSerializer
+# /class BidCreateView(ListCreateAPIView):
+#     serializer_class=BidSerializer
+#     def perform_create(self, serializer):
+#         # Assign logged-in user as seller before saving
+#         serializer.save(buyer=self.request.user)
+# class BidUpdateView(ListCreateAPIView):
+#     serializer_class=BidSerializer
+#     def perform_create(self, serializer):
+#         # Assign logged-in user as seller before saving
+#         serializer.save(buyer=self.request.user)
+#     def get_queryset(self):
+#         return Bid.objects.all()
+#     def put(self,request,pk):
+#         bid=Bid.objects.get(pk=pk)
+#         serializer=BidSerializer(bid,data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse({
+#                 "msg":"bid updated successfully"
+#             })
+#         return JsonResponse({
+#             "msg":"error in updating bid"
+#         })
 
 # class placebidview():
 #     def post(self,request):
